b4357a8440dba5674faa39f3dfdd15c51f41ee3d
terry255180 <119744133+terry255180@users.noreply.github.com>
2023-12-02 03:14:49 +0800

    Update app.py

--- examples/flask-kitchensink/app.py
+++ examples/flask-kitchensink/app.py
@@ -802,20 +802,17 @@ def handle_file_message(event):
             )
         )
 
-MSG = [{
-      type: 'text',
-      text: '❤️歡迎各位孕媽咪、寶媽咪❤️'
-    }]
 @handler.add(FollowEvent)
 def handle_follow(event):
     app.logger.info("Got Follow event:" + event.source.user_id)
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
+        Join_text = "我加入了～"
         line_bot_api.reply_message(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
                 #messages=[TextMessage(text='Got follow event')]
-                #messages=[TextMessage(text=MSG)]
+                messages=[TextMessage(text=Join_text)]
             )
         )
 
