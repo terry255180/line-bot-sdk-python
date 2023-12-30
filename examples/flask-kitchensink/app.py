c493020c07bab0c1ae0f9d6bdd65b088deabf40d
terry255180 <119744133+terry255180@users.noreply.github.com>
2023-12-30 22:46:09 +0800

    Update app.py

--- examples/flask-kitchensink/app.py
+++ examples/flask-kitchensink/app.py
@@ -123,6 +123,25 @@ configuration = Configuration(
     access_token=channel_access_token
 )
 
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except ApiException as e:
+        app.logger.warn("Got exception from LINE Messaging API: %s\n" % e.body)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
 @handler.add(FollowEvent)
 def handle_follow(event):
     app.logger.info("Got Follow event:" + event.source.user_id)
